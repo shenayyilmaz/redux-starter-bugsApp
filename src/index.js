e31932e294c78940a695fcdf9f2e42892520af58
GJQ Holdings Mac1 <shenay_yilmaz@hotmail.com>
2020-04-23 15:47:43 +0100

    install redux devTools

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import store from "./store/configureStore";
 import { addBug, removeBug, resolvedBug } from "./actions/bugs";
 
-import store3 from "./reducerFromScratch/customStore";
+// import store3 from "./reducerFromScratch/customStore";
 
-store3.subscribe(() => {
-  console.log("store is changed");
-});
+// store3.subscribe(() => {
+//   console.log("store is changed");
+// });
 
-store3.dispatch(addBug("bug 1111"));
-console.log("store", store3.getSate());
+// store3.dispatch(addBug("bug 1111"));
+// console.log("store", store3.getSate());
 
 // store.subscribe(() => {
 //   console.log("store is change", store.getState());
 // });
 
-// store.dispatch(addBug("BUG 3"));
-// store.dispatch(addBug("BUG 4"));
+store.dispatch(addBug("BUG 3"));
+store.dispatch(addBug("BUG 4"));
 
-// store.dispatch(resolvedBug(2));
-// store.dispatch(removeBug(1));
+store.dispatch(resolvedBug(2));
+store.dispatch(removeBug(1));
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
 import bugReducer from "../reducers/bugs";
 
-const store = createStore(bugReducer);
+const store = createStore(
+  bugReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
